ec87b558cc57c889d8c02750bf0ae2b922b8a921
Nicole <nalbrigh@kent.edu>
2019-03-19 11:26:19 -0400

    trying to style with bootstrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,18 +9,17 @@ var config = {
     projectId: "train-schedule-wk7-11d97",
     storageBucket: "train-schedule-wk7-11d97.appspot.com",
     messagingSenderId: "16070555178"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
-
 
 //form on click function
 //preventDefault prevent page from reloading 
 $("#add-employee-btn").on("click", function () {
     event.preventDefault();
 
-//takes values from input boxes and trims them 
+    //takes values from input boxes and trims them 
     name = $("#employee-name-input").val().trim();
     destination = $("#role-input").val().trim();
     arrivalTime = $("#start-input").val().trim();
@@ -38,25 +37,34 @@ $("#add-employee-btn").on("click", function () {
     database.ref().push({
         name: name,
         destination: destination,
-       arrivalTime: arrivalTime,
-       frequency: frequency,
-       
+        arrivalTime: arrivalTime,
+        frequency: frequency,
+
     });
 
-    //alerts the user they successfully added a train schedule 
-    alert("New Train is on the way!");
     
-//empties  the inut box value 
+        if (name === "" || destination === "" || arrivalTime === "" || frequency === "") {
+            $("#invalidEntry").text("Please Enter a Valid Entry.");
+        }
+
+     
+          //alerts the user they successfully added a train schedule 
+   
+   
+
+           //empties  the inut box value 
+        
     $("#employee-name-input").val("");
     $("#role-input").val("");
     $("#start-input").val("");
     $("#rate-input").val("");
+    
 });
 
 
 //
-database.ref().on("child_added", function (document){
-   //sets counter equal to 1 
+database.ref().on("child_added", function (document) {
+    //sets counter equal to 1 
     count += 1;
 
     console.log(document.key);
@@ -76,37 +84,32 @@ database.ref().on("child_added", function (document){
     // var time = moment(arrivalTime, "HH:MM").subtract(1, "years");
     var time = moment().hours(timeArray[0]).minutes(timeArray[1]);
 
-
-
     //your problems is right below 
-    var difference =  now.diff(time, "minutes");
+    var difference = now.diff(time, "minutes");
     var total = difference % frequency;
-    trainArrivalMinutes =  frequency - total;
+    trainArrivalMinutes = frequency - total;
 
     console.log(difference);
     console.log(total);
-     console.log(time);
-     
-     
+    console.log(time);
 
+    var nextTrain = moment().add(trainArrivalMinutes, "minutes");
+    trainArrivalTime = moment(nextTrain).format("HH:MM");
 
-var nextTrain = moment().add(trainArrivalMinutes, "minutes");
-trainArrivalTime = moment(nextTrain).format("HH:MM");
+    var a = "<a href=# onclick=deleteDocument('" + document.key + "');>Delete</a>";
 
-var a = "<a href=# onclick=deleteDocument('" + document.key + "');>Delete</a>";
+    //appends new train information into table 
+    $("#employee-table").append(
+        $("<tr>").append(
+            $("<td>").text(name),
+            $("<td>").text(destination),
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrain.format("hh:mm")),
+            $("<td>").text(trainArrivalMinutes),
+            $("<td>").html(a)
+        ));
 
-//appends new train information into table 
-$("#employee-table").append(
-    $("<tr>").append(
-        $("<td>").text(name),
-        $("<td>").text(destination),
-        $("<td>").text(frequency),
-        $("<td>").text(nextTrain.format("hh:mm")),
-        $("<td>").text(trainArrivalMinutes),
-        $("<td>").html(a)
-    ));
-
-console.log("Record:" + count);
+    console.log("Record:" + count);
 
 });
 
